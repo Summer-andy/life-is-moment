d5b1e3db87e0eb17c14eb63c7304dc4714e3a653
xdSi <3450236968@qq.com>
2020-02-20 11:43:59 +0800

    docs: 修改deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
+#!/bin/bash
 git checkout master
 git pull
 npm run docs:build
-scp -r ./docs/.vuepress/dist/. root@139.196.82.33:/webSrc
+scp -r ./docs/.vuepress/dist/. root@139.196.82.33:/webSrc/dist
